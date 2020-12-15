fd7d4db770a0a06e2a70185f23553c5a5fd7a3f5
Ascarov Ilgizar <ffox07@mail.ru>
2020-12-16 00:01:05 +0300

    [add] id заявки

--- modules/api/serializer/advance/ActiveAdvanceSerializer.php
+++ modules/api/serializer/advance/ActiveAdvanceSerializer.php
@@ -14,6 +14,7 @@ class ActiveAdvanceSerializer extends AbstractProperties
     {
         return [
             Advance::class => [
+                'id',
                 'issue_date' => function(Advance $advance) {
                     return AdvanceIssueDateFormatter::formatter($advance);
                 },
