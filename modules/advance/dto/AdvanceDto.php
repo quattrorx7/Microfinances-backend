f96be962c6b566b472d01e37b921ae4faf2dcbaf
Ascarov Ilgizar <ffox07@mail.ru>
2020-12-21 15:11:23 +0300

    [change] сообщение в рассчет процентов

--- modules/advance/dto/AdvanceDto.php
+++ modules/advance/dto/AdvanceDto.php
@@ -3,6 +3,7 @@
 namespace app\modules\advance\dto;
 
 use app\components\exceptions\UserException;
+use app\helpers\PriceHelper;
 
 class AdvanceDto
 {
@@ -28,7 +29,8 @@ class AdvanceDto
         $this->summa_with_percent = round($daily_payment * $limitation);
 
         if ($this->summa_with_percent < $this->amount) {
-            throw new UserException('Сумма с процентами ' . $this->summa_with_percent . ' не может быть меньше суммы займа '. $amount);
+            $daily_payment = ceil($this->amount / $limitation);
+            throw new UserException('Минимальная сумма ' . PriceHelper::priceFormat($daily_payment) . ' - это займ поделенный на кол-во дней');
         }
 
         $this->percent = round(100 - 100 * $amount / $this->summa_with_percent, 2);
