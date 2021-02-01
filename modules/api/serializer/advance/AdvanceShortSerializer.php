9851f1294c2f2fb8bb6d7cd44f49a21c8dc6cd53
Ascarov Ilgizar <ffox07@mail.ru>
2021-02-01 13:01:45 +0300

    [fix]

--- modules/api/serializer/advance/AdvanceShortSerializer.php
+++ modules/api/serializer/advance/AdvanceShortSerializer.php
@@ -4,6 +4,7 @@ namespace app\modules\api\serializer\advance;
 
 use app\models\Advance;
 use app\components\serializers\AbstractProperties;
+use app\modules\advance\components\AdvanceRepository;
 use app\modules\advance\formatters\AdvanceIssueDateFormatter;
 
 class AdvanceShortSerializer extends AbstractProperties
@@ -17,7 +18,17 @@ class AdvanceShortSerializer extends AbstractProperties
                 'issue_date' => function(Advance $advance) {
                     return AdvanceIssueDateFormatter::formatter($advance);
                 },
-                'amount'
+                'amount',
+                'refinancing' => function(Advance $model) {
+                    if($model->isRefinancing()){
+                        $ids = $model->refinancingIds();
+                        $repository = new AdvanceRepository();
+                        $advances = $repository->getRefinancingById($ids);
+                        return ActiveAdvanceSerializer::serialize($advances);
+                    }else{
+                        return null;
+                    }
+                }
             ]
         ];
     }
